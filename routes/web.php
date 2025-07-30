231018bcb3456308983db1c416ceea78e894ea3d
gbagiang <garik.bagdasaryan.24@gmail.com>
2025-07-30 23:57:16 +0300

    [UPDATE] Настройка маршрутизации

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/monumentsPage', function () {
+    return view('monumentsPage');
+})->name('monumentsPage');
+
+Route::get('/newsPage', function () {
+    return view('newsPage');
+})->name('newsPage');
+
+Route::get('/appsPage', function () {
+    return view('appsPage');
+})->name('appsPage');
