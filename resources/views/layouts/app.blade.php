e5c4131da9d63f3ace6acf8db30aefa56c6a0512
gbagiang <garik.bagdasaryan.24@gmail.com>
2025-07-30 17:47:16 +0300

    [ADD] Добавление структуры стилей, шрифтов, картинок

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', config('app.name'))</title>
+    @vite(['resources/scss/app.scss'])
+</head>
+
+<body>
+    <header>
+        <div>
+            <a href="{{ url('/') }}">
+                {{ config('app.name', 'LaravelApp') }}
+            </a>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <div>
+            &copy; {{ date('Y') }} {{ config('app.name') }}. Все права защищены.
+        </div>
+    </footer>
+
+    @vite(['resources/js/app.js'])
+</body>
+
+</html>
